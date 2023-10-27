7b0eac1f1631231aebac809b86bfa5c6d018d8e6
Anita Sharma <sharma.anita00001@gmail.com>
2023-10-27 11:15:52 +0545

    Greeting Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Greeting />
     </div>
   );
 }
